99ce388e4e71d4d2144dccf23b05668c0fecbfd9
Oncu <oohancan@gmail.com>
2022-09-02 08:51:58 +0300

    add FruitRowView

---
+++ Fructus/View/FruitRowView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitRowView.swift
+//  Fructus
+//
+//  Created by Oncu Can on 2.09.2022.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitRowView()
+    }
+}
