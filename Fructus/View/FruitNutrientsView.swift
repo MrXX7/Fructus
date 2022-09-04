5d1647e4be8cd28ad4054770f38e916ebd9b244f
Oncu <oohancan@gmail.com>
2022-09-04 15:44:03 +0300

    add FruitNutrientsView

---
+++ Fructus/View/FruitNutrientsView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitNutrientsView.swift
+//  Fructus
+//
+//  Created by Oncu Can on 4.09.2022.
+//
+
+import SwiftUI
+
+struct FruitNutrientsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitNutrientsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitNutrientsView()
+    }
+}
